f1f42e6b3891d040e955b3015c15ec71d8d01b00
gabriel-eb <gabo_espburg@hotmail.com>
2025-12-11 14:40:35 -0500

    add base for gh actions

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import glsl from "vite-plugin-glsl";
 export default {
   root: "src/",
   publicDir: "../static/",
-  base: "./",
+  base: "/luma16-navidad-2025/",
   server: {
     host: true,
     open: true,
